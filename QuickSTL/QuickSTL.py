7564f424cea7c30370fb6452307bf41ebdfc5ff0
Bill Nguyen <kancerstudios@gmail.com>
2026-01-11 23:11:04 -0800

    Fix add-in entrypoint import path

--- QuickSTL/QuickSTL.py
+++ QuickSTL/QuickSTL.py
@@ -1,5 +1,12 @@
 """Fusion 360 entrypoint for the QuickSTL add-in."""
 
+import os
+import sys
+
+ADDIN_DIR = os.path.dirname(os.path.realpath(__file__))
+if ADDIN_DIR not in sys.path:
+    sys.path.insert(0, ADDIN_DIR)
+
 from quickstl.entrypoint import run, stop
 
 __all__ = ["run", "stop"]
